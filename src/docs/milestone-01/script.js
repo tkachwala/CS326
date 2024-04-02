92854d59894cdcafb6cde294f5ef526cf1cc044f
sonitbandaru <you@example.com>
2024-04-02 00:26:36 -0400

    Updated script.js

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,16 +1,21 @@
-const element = document.getElementById('vision');
-const text = 'Locality';
+document.addEventListener('DOMContentLoaded', () => {
+    const startButton = document.getElementById('startButton');
+    const element = document.getElementById('vision');
+    const text = 'Locality';
+    let index = 0;
 
-let index = 0;
-
-function typeEffect() {
-    if (index < text.length) {
-        element.textContent += text.charAt(index);
-        index++;
-        setTimeout(typeEffect, 150); 
-    } else {
-        element.style.borderRight = 'none'; 
+    function typeEffect() {
+        if (index < text.length) {
+            element.textContent += text.charAt(index);
+            index++;
+            setTimeout(typeEffect, 150); 
+        } else {
+            element.style.borderRight = 'none'; 
+        }
     }
-}
-
-typeEffect();
+    startButton.addEventListener('click', () => {
+        index = 0;
+        element.textContent = '';
+        typeEffect();
+    });
+});
