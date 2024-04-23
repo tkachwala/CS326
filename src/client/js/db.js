65be351d4594195da51b30ac16462f94cb38dd42
Anushka Trehan <atrehan@umass.edu>
2024-04-23 19:39:32 -0400

    changes

--- src/client/js/db.js
+++ src/client/js/db.js
@@ -1,9 +1,7 @@
 // Import PouchDB as an ES module
 import PouchDB from 'https://cdn.skypack.dev/pouchdb-browser';
 
-// Initialize the database
 const db = new PouchDB('my_database');
 console.log('Database initialized', db);
 
-// Export the database instance for use in other modules
 export default db;
