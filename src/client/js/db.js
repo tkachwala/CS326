2ae00ca2f570a17c64f28ae5c67daddac72e4ec6
taher kachwala <tkachwala@umass.edu>
2024-04-25 01:19:20 -0400

    Readme and documentation

--- src/client/js/db.js
+++ src/client/js/db.js
@@ -1,7 +1,9 @@
 // Import PouchDB as an ES module
 import PouchDB from 'https://cdn.skypack.dev/pouchdb-browser';
 
+// Initialize PouchDB database
 const db = new PouchDB('my_database');
 console.log('Database initialized', db);
 
+// Export the PouchDB database instance
 export default db;
